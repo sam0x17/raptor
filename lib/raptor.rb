fe555fa64c652602ce6cea5f7209f456352f5232
Sam Kelly <sam@durosoft.com>
2015-06-25 15:24:29 -0400

    Fix issue with duplicate poses.

--- lib/raptor.rb
+++ lib/raptor.rb
@@ -29,35 +29,30 @@ module RAPTOR
   end
 
   def self.experiment(img_dir, interpolate=false, euclidean_error=false)
-    puts "Experiment started using #{img_dir}"
+    puts "Experiment started using images contained in '#{Dir.pwd}/#{img_dir}'"
     $gh = RAPTOR::GridHash.new
     $gh.process_images(img_dir)
-    imgs = []
-    puts "Loading list of images..."
-    Dir.glob("#{img_dir}/**/*.png") do |file|
-      imgs << file
-    end
-    puts "Ensuring that image set is unique..."
-    imgs.uniq!
-    puts "Randomly shuffling image set..."
+    puts "Shuffling image set..."
+    imgs = $gh.image_files.clone
     imgs.shuffle!
-    puts "Performing tests..."
     test_set = imgs.first(1000)
+    test_set.uniq!
     avg_err = 0.0
+    already_exist_count = 0
+    puts "Running experiment..."
     test_set.each do |img_path|
-      puts "Testing #{img_path}..."
-      counts = $gh.identify_rotation(img_path).to_a.last(8)
-      expected = counts.last[0]
-      puts "expected: |#{expected}|"
-      expected_str = $gh.get_file_index_by_rotation(expected)
+      expected_str = File.basename(img_path, ".png")
+      counts = $gh.identify_rotation(img_path).last(8)
       test_dir = "experiment_output/#{expected_str}"
-      puts "attempting to make directory #{test_dir}"
+      expected_rot_id = expected_str.to_i - 1
+      expected = $gh.rotation_by_id(expected_rot_id)
       Dir.mkdir(test_dir)
       i = counts.size - 1
       counts.each do |arr|
         rot = arr[0]
-        count = arr[1]
-        item_str = $gh.get_file_index_by_rotation(rot).to_s.rjust(7, "0")
+        rot_id = arr[1] + 1
+        count = arr[2]
+        item_str = rot_id.to_s.rjust(7, "0")
         error = RAPTOR.rotation_percent_error(expected, rot) if !euclidean_error
         error = RAPTOR.rotation_euclidean_error(expected, rot) if euclidean_error
         FileUtils.cp("#{img_dir}/#{item_str}.png", "#{test_dir}/#{i}.png")
@@ -69,17 +64,19 @@ module RAPTOR
         real = counts[counts.size - 1]
         guess1 = counts[counts.size - 2]
         guess2 = counts[counts.size - 3]
-        interp = RAPTOR.interpolate(guess1[0], guess2[0], guess1[1], guess2[1])
+        interp = RAPTOR.interpolate(guess1[0], guess2[0], guess1[2], guess2[2])
         error = RAPTOR.rotation_percent_error(real[0], interp) if !euclidean_error
         error = RAPTOR.rotation_euclidean_error(real[0], interp) if euclidean_error
         puts "#{error}\t#{interp}"
         avg_err += error
       end
     end
+    puts "already existed: #{already_exist_count}"
     puts "# tests: #{test_set.size}"
     puts "RAPTOR memory usage: #{$gh.memory_size} bytes"
     avg_err = avg_err / test_set.size.to_f
-    puts "Average % error: #{avg_err}"
+    puts "Average % error: #{avg_err}" if !euclidean_error
+    puts "Average deviation: #{avg_error}" if euclidean_error
     true
   end
 
