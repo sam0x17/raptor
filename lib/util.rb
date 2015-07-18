7a64ad84550b3a573baa2b2de118f5220961a060
Sam Kelly <sam@durosoft.com>
2015-07-17 21:48:02 -0400

    Add image gradient functionality.

--- lib/util.rb
+++ lib/util.rb
@@ -1,3 +1,5 @@
+
+
 def smart_resize_bounds(sw, sh, dw, dh)
   raise "dest width and dest height cannot both be nil!"  if dw.nil? && dh.nil?
   voff = 0.0; hoff = 0.0
@@ -23,4 +25,54 @@ def smart_resize_bounds(sw, sh, dw, dh)
     break
   end
   {x: x, y: y, w: w, h: h}
+end
+
+def get_image_gradient(img, grayscale=false)
+  img = ChunkyPNG::Image.from_file(img) if img.is_a? String
+  width = img.dimension.width
+  height = img.dimension.height
+  img2 = img.clone
+  width.times do |x|
+    height.times do |y|
+      orig_color = img2[x, y]
+      next if orig_color == 0
+      vpos = Proc.new do |tx, ty|
+        ret = nil
+        if tx >= 0 && tx < width && ty >= 0 && ty < height
+          ret = [tx, ty]
+        else
+          ret = [x, y]
+        end
+        ret
+      end
+      points = []
+      points << vpos.(x - 1, y)
+      points << vpos.(x - 1, y - 1)
+      points << vpos.(x, y - 1)
+      colors = []
+      points.each {|point| colors << ChunkyPNG::Color.to_truecolor_bytes(img2[point[0], point[1]])}
+      orig_color_bytes = ChunkyPNG::Color.to_truecolor_bytes(orig_color)
+      avg_diff = [0.0, 0.0, 0.0]
+      colors.each do |color|
+        diff = [(color[0] - orig_color_bytes[0]).abs,
+                (color[1] - orig_color_bytes[1]).abs,
+                (color[2] - orig_color_bytes[2]).abs]
+        avg_diff[0] += diff[0].to_f
+        avg_diff[1] += diff[1].to_f
+        avg_diff[2] += diff[2].to_f
+      end
+      avg_diff[0] = (avg_diff[0] / points.size).round
+      avg_diff[1] = (avg_diff[1] / points.size).round
+      avg_diff[2] = (avg_diff[2] / points.size).round
+      if grayscale
+        a = ((avg_diff[0].to_f + avg_diff[1].to_f + avg_diff[2].to_f) / 3.0).round
+        avg_diff = [a, a, a]
+      end
+      img[x, y] = ChunkyPNG::Color.rgba(avg_diff[0], avg_diff[1], avg_diff[2], 255)
+    end
+    #puts "x: #{x}" if x % 100 == 0
+  end
+  #puts "saving image..."
+  #img.save('test_output.png')
+  img
 end
